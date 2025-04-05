5b46004faeaa51c672d34a3f210120ed69f2f6e0
Muhammad Ammar <muhammadammar4460@gmail.com>
2025-04-05 22:34:51 +0500

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -7,7 +7,7 @@ import project6 from "../assets/projects/project-6.png";
 
 export const HERO_CONTENT = `I’m a passionate software developer specializing in Artificial Intelligence, with a focus on training AI models, optimizing ANNs, and applying NLP and Computer Vision for real-world solutions. Alongside AI, I build dynamic, scalable web apps using the MERN stack. I’m skilled in solving complex problems using data structures, algorithms, and MySQL. My goal is to create efficient, intelligent, and impactful tech solutions.`
 
-export const ABOUT_TEXT = `Driven by a passion for AI-driven solutions and full-stack development, I thrive in collaborative environments where innovation and creativity take center stage. I actively engage in hands-on projects, refining my skills in debugging, performance optimization, and real-world application development.
+export const ABOUT_TEXT = `3rd year student at FAST NUCES with a passion for AI-driven solutions and full-stack development, I thrive in collaborative environments where innovation and creativity take center stage. I actively engage in hands-on projects, refining my skills in debugging, performance optimization, and real-world application development.
 I am committed to continuous learning and staying updated with emerging trends in AI and software development. My goal is to leverage my knowledge and expertise to build impactful solutions that drive technological advancement and solve meaningful challenges.`;
 
 export const EDUCATION = [
